45055cab015a5333a810f87f17f166d1358651f7
Deepakreddy <32331264+deepakreddy5511@users.noreply.github.com>
2021-06-27 18:03:49 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,28 +1,8 @@
-import re
-
-print("Welcome to our calculator")
-last = 0
-run = True
-
-
-def calculation():
-    global run
-    global last
-    equation = ""
-    if last == 0:
-        equation = input("Please Enter A Equation:")
-    else:
-        equation = input(str(last))
-    if equation == 'quit':
-        run = False
-    else:
-        equation1 = re.sub("[a-zA-Z.,':]", "", equation)
-        if last==0:
-            last=eval(equation1)
-        else:
-            last = eval(str(last) + equation1)
-    print("Your calculation is:", last)
-
-
-while run == True:
-    calculation()
+weight = int(input("Weight:"))
+unit = input("Enter LBS or KGS:")
+if unit == "LBS":
+    convert = weight * 0.45
+    print(f'you are {convert} KGS')
+else:
+    convert = weight / 0.45
+    print(f'you are {convert} LBS')
